73b62b26540d191b4a779de69ce3c781b51062dc
llmlks <lamelkas@gmail.com>
2018-02-10 15:03:27 +0200

    Frontend to Heroku

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 app.use(bodyParser.json())
 app.use(cors())
+app.use(express.static('build'))
 app.set('json spaces', 4)
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
