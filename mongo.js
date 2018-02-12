fc4e647f8712eee2980f64410d40708e35f4e8e3
llmlks <lamelkas@gmail.com>
2018-02-12 10:20:05 +0200

    Separate dev DB

--- mongo.js
+++ mongo.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose')
-const url = MONGODB_URI
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
 
